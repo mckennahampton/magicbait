240a00bf6f6e018bae089a9a14c709a6793860eb
McKenna Hampton <mckenna@mckennahampton.com>
2018-10-04 16:06:49 -0500

    Added slider to front page

---
+++ Presentation/Nop.Web/Themes/MagicBait/Views/Shared/_HomePage.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nop.Web.Themes.MagicBait.Views.Shared
+{
+    public class _HomePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
