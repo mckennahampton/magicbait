e7ffc5855fa096c226ecde7a06bc311135398cbb
McKenna Hampton <mckenna@mckennahampton.com>
2018-10-10 18:08:20 -0500

    Started building the shop page

---
+++ Presentation/Nop.Web/Themes/MagicBait/Views/Topic/_ChildCategories.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nop.Web.Themes.MagicBait.Views.Topic
+{
+    public class _ChildCategoriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
